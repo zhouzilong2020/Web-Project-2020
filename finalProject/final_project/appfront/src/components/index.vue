0cca1a7253844942076941ed25c73ad0e7c744be
zhouzilong2020 <529620861@qq.com>
2020-08-24 22:01:49 +0800

    index page

--- finalProject/final_project/appfront/src/components/index.vue
+++ finalProject/final_project/appfront/src/components/index.vue
@@ -1,11 +1,47 @@
 <template>
-  <q-page class="flex flex-center">
-    
+  <q-page class="index  absolute-center">
+    <q-carousel
+        v-model="slide"
+        transition-prev="scale"
+        transition-next="scale"
+        swipeable
+        animated
+        infinite
+        autoplay
+        control-color="white"
+        navigation
+        padding
+        arrows
+        height="300px"
+        class="bg-primary text-white shadow-1 rounded-borders"
+      >
+        <q-carousel-slide name="style" class="column no-wrap flex-center">
+          <q-icon name="style" size="56px" />
+          <div class="q-mt-md text-center">
+            {{ p1 }}
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide name="tv" class="column no-wrap flex-center">
+          <q-icon name="live_tv" size="56px" />
+          <div class="q-mt-md text-center">
+            {{ p2 }}
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide name="layers" class="column no-wrap flex-center">
+          <q-icon name="layers" size="56px" />
+          <div class="q-mt-md text-center">
+            {{ p3 }}
+          </div>
+        </q-carousel-slide>
+      </q-carousel>
+    <q-btn-group spread>
+      <q-btn color="purple" label="马上登录" icon="homepage" :to="{name:'login'}" />
+      <q-btn color="purple" label="现在注册" icon="visibility" :to="{name:'register'}"/>
+    </q-btn-group>
+  
+  
   </q-page>
 </template>
-
-<style>
-</style>
 
 <script>
 import {mapState} from 'vuex'
@@ -13,7 +49,10 @@ export default {
   name: 'HelloWorld',
   data(){
     return {
-      
+      slide: 'style',
+      p1:'使用GoodRead 一页尽知天下事',
+      p2:'一个Web 就是一个宇宙',
+      p3:'一节课里可以学到 PHP HTML5 ES6+ JAVASCRIPT 真好！'    
     }
   },
   computed: mapState('userInfo', ['userInfo']),
@@ -32,3 +71,11 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.index{
+  max-width: 800px;
+  width:80%;
+  margin:200px auto;
+}
+</style>
