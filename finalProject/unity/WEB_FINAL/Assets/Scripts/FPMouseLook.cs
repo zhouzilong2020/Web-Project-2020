037e2a926aa0bcf7ab7556775e4601d4ccbe19b0
zhouzilong2020 <529620861@qq.com>
2020-08-26 21:19:02 +0800

    init unity

---
+++ finalProject/unity/WEB_FINAL/Assets/Scripts/FPMouseLook.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPMouseLook : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private 
+    // Update is called once per frame
+    void Update()
+    {// 获取输入
+        var temp_MouseX = Input.GetAxis("mouse X");
+        var temp_MouseY = Input.GetAxis("mouse Y");
+        
+        // temp_Mouse
+    }
+}
